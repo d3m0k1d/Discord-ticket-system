d9affb9dbabf7f146a0a3f9f81b441e26ce1a761
d3m0k1d <d3m0k1d1337@gmail.com>
2025-08-23 22:08:20 +0300

    Add Prometheus metrics to API gateway and Docker integration

--- cmd/api-gateway/main.go
+++ cmd/api-gateway/main.go
@@ -4,12 +4,13 @@ import (
 	_ "net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
 
 	r := gin.Default()
-
+	r.GET("/metrics", gin.WrapH(promhttp.Handler()))
 	v1 := r.Group("/api/v1")
 	{
 		v1.GET("/tickets", ListTickets)
@@ -17,6 +18,6 @@ func main() {
 		v1.POST("/tickets", CreateTicket)
 		v1.DELETE("/tickets/:id", DeleteTicket)
 	}
-	r.Run(":8080")
+	r.Run("0.0.0.0:8080")
 
 }
